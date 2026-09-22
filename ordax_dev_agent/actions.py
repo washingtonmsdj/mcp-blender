80124b03faf6d29647842c4c49044e0333bc079f
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-22 17:55:17 -0300

    feat: create solid Blender boxes with typed cutouts
    * feat: add typed box cutout modeling action
    
    * feat: register box cutout modeling action
    
    * feat: create solid Blender boxes with real cutouts
    
    * test: cover typed box cutout modeling action
    
    * chore: bump Dev Agent to 1.19.41

--- ordax_dev_agent/actions.py
+++ ordax_dev_agent/actions.py
@@ -73,6 +73,7 @@ class ActionRegistry(
             "blender.live_object_remove": self.blender_live_object_remove,
             "blender.live_extract_region": self.blender_live_extract_region,
             "blender.live_create_primitive": self.blender_live_create_primitive,
+            "blender.live_create_box_with_cutouts": self.blender_live_create_box_with_cutouts,
             "blender.live_add_modifier": self.blender_live_add_modifier,
             "blender.live_material_apply": self.blender_live_material_apply,
             "blender.live_create_camera": self.blender_live_create_camera,
