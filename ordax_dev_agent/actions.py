bfa64332825744a17df96c8d9217a3cd1cd815f9
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-24 22:19:32 -0300

    feat: register verified engine export Unity handoff

--- ordax_dev_agent/actions.py
+++ ordax_dev_agent/actions.py
@@ -160,6 +160,7 @@ class ActionRegistry(
             "game_assets.blender_export_verified": self.game_assets_blender_export_verified,
             "game_assets.engine_export_verify": self.game_assets_engine_export_verify,
             "game_assets.unity_import_generated": self.game_assets_unity_import_generated,
+            "game_assets.unity_import_engine_export": self.game_assets_unity_import_engine_export,
             "game_assets.unity_model_audit": self.game_assets_unity_model_audit,
             "game_assets.unity_build_static_lod_prefab": self.game_assets_unity_build_static_lod_prefab,
             "game_assets.blender_character_preflight": self.game_assets_blender_character_preflight,
