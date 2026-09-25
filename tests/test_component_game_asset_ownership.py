96ba19f68e8f3dbb5569078015f673d7857f824c
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-24 22:20:08 -0300

    test: include engine export provenance in game asset ownership

--- tests/test_component_game_asset_ownership.py
+++ tests/test_component_game_asset_ownership.py
@@ -9,6 +9,7 @@ class GameAssetComponentOwnershipTests(unittest.TestCase):
             [
                 "ordax_dev_agent/game_asset_image_actions.py",
                 "ordax_dev_agent/game_asset_artifact_actions.py",
+                "ordax_dev_agent/game_asset_engine_export_actions.py",
                 "ordax_dev_agent/game_asset_status_actions.py",
                 "ordax_dev_agent/game_asset_runtime_actions.py",
                 "ordax_dev_agent/game_asset_lod_actions.py",
