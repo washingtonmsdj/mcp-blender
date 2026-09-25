df1fdb519ad817e2b3c75e5e1da802935052fc9d
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-24 22:50:18 -0300

    test: include Unreal adapter in game asset ownership

--- tests/test_component_game_asset_ownership.py
+++ tests/test_component_game_asset_ownership.py
@@ -15,6 +15,7 @@ class GameAssetComponentOwnershipTests(unittest.TestCase):
                 "ordax_dev_agent/game_asset_runtime_actions.py",
                 "ordax_dev_agent/game_asset_lod_actions.py",
                 "ordax_dev_agent/game_asset_unity_actions.py",
+                "ordax_dev_agent/game_asset_unreal_actions.py",
                 "ordax_dev_agent/generated_asset_actions.py",
                 "ordax_dev_agent/assets/blender_generated_asset_ingest.py",
                 "ordax_dev_agent/assets/blender_runtime_budget.py",
