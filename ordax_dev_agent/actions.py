be68619372f8334b5ceef76da35c6fa68f0aa3dc
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-25 00:18:47 -0300

    feat: expose Unity animation sample audit

--- ordax_dev_agent/actions.py
+++ ordax_dev_agent/actions.py
@@ -36,6 +36,7 @@ from .game_asset_runtime_actions import GameAssetRuntimeActions
 from .game_asset_unity_actions import GameAssetUnityActions
 from .game_asset_unity_semantic_actions import GameAssetUnitySemanticActions
 from .game_asset_unity_import_config_actions import GameAssetUnityImportConfigActions
+from .game_asset_unity_animation_actions import GameAssetUnityAnimationActions
 from .game_asset_unreal_actions import GameAssetUnrealActions
 from .game_asset_unreal_semantic_actions import GameAssetUnrealSemanticActions
 from .visual_environment_actions import VisualEnvironmentActions
@@ -78,6 +79,7 @@ class ActionRegistry(
     GameAssetUnityActions,
     GameAssetUnitySemanticActions,
     GameAssetUnityImportConfigActions,
+    GameAssetUnityAnimationActions,
     GameAssetUnrealActions,
     GameAssetUnrealSemanticActions,
     VisualEnvironmentActions,
@@ -194,6 +196,7 @@ class ActionRegistry(
             "game_assets.unity_model_audit": self.game_assets_unity_model_audit,
             "game_assets.unity_semantic_audit": self.game_assets_unity_semantic_audit,
             "game_assets.unity_character_import_configure": self.game_assets_unity_character_import_configure,
+            "game_assets.unity_animation_sample_audit": self.game_assets_unity_animation_sample_audit,
             "game_assets.unity_build_static_lod_prefab": self.game_assets_unity_build_static_lod_prefab,
             "game_assets.blender_character_preflight": self.game_assets_blender_character_preflight,
             "game_assets.blender_export": self.game_assets_blender_export,
